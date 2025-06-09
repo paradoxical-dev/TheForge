9cb6f0f805d61c3063c68168dcc88904eb5859f8
jaredonnell <jaredonnell21@gmail.com>
2025-06-09 13:54:25 -0500

    Update: experimental tabs

--- theforge/screens/settings.py
+++ theforge/screens/settings.py
@@ -1,6 +1,6 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Footer, Static
+from textual.widgets import Footer, Static, TabbedContent, TabPane
 
 
 class SecondScreen(Screen):
@@ -8,7 +8,12 @@ class SecondScreen(Screen):
 
     def compose(self) -> ComposeResult:
         yield Footer()
-        yield Static("Second Page")
+        # yield Tabs("First tab", "Second tab", "[u]Third[/u] tab")
+        with TabbedContent(initial="test1"):
+            with TabPane("test1", id="test1"):
+                yield Static("Hello")
+            with TabPane("test2"):
+                yield Static("Bye")
 
     def action_go_home(self):
         self.app.action_go_home()
