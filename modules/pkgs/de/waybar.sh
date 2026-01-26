34e9b51bec2adea773ee44dd376ea0a207073751
paradoxical-dev <jaredonnell21@gmail.com>
2026-01-25 23:05:36 -0600

    feat(waybar): unmasked waybar for latest version

--- modules/pkgs/de/waybar.sh
+++ modules/pkgs/de/waybar.sh
@@ -6,6 +6,7 @@ echo "Installing waybar..."
 echo ""
 
 edit_use "waybar" "gui-apps/waybar" "mpris mpd wifi network backlight tray"
+unmask_pkg "waybar" "gui-apps/waybar" "~amd64"
 
 install_pkg "waybar" "gui-apps/waybar"
 echo ""
