585c0a11a637f4a9acf206f34b97cdc82e1f5780
paradoxical-dev <jaredonnell21@gmail.com>
2026-02-04 12:18:50 -0600

    fix(carapace): added check for nushell cache dir

--- modules/pkgs/cli/carapace.sh
+++ modules/pkgs/cli/carapace.sh
@@ -8,5 +8,9 @@ echo ""
 unmask_pkg "carapace" "app-shells/carapace" "~amd64"
 echo ""
 
+if [[ ! -d "$HOME/.cache/nushell" ]]; then
+    mkdir "$HOME/.cache/nushell"
+fi
+
 install_pkg "carapace" "app-shells/carapace"
 echo ""
