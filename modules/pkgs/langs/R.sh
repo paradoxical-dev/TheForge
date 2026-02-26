9acdb0aa842525cc005e71131a8d5560ad596b0c
paradoxical-dev <jaredonnell21@gmail.com>
2026-02-26 13:48:16 -0600

    feat(R): updated USE flags

--- modules/pkgs/langs/R.sh
+++ modules/pkgs/langs/R.sh
@@ -5,5 +5,7 @@ set -e
 echo "Installing R..."
 echo ""
 
+edit_use "R" "dev-lang/R" "cairo jpeg png"
+
 install_pkg "R" "dev-lang/R"
 echo ""
--- modules/pkgs/langs/gnuplot.sh
+++ modules/pkgs/langs/gnuplot.sh
@@ -5,5 +5,7 @@ set -e
 echo "Installing gnuplot..."
 echo ""
 
+edit_use "gnuplot" "sci-visualization/gnuplot" "cairo"
+
 install_pkg "gnuplot" "sci-visualization/gnuplot"
 echo ""
