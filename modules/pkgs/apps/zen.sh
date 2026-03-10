708360cbd2e965e744475710305ae633ea73fbcf
paradoxical-dev <jaredonnell21@gmail.com>
2026-03-10 12:12:38 -0500

    feat(moduels): added zen and httrack modules

---
+++ modules/pkgs/apps/zen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Zen browser..."
+echo ""
+
+install_pkg "zen" "www-client/zen-bin"
+echo ""
---
+++ modules/pkgs/cli/httrack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing httrack..."
+echo ""
+
+install_pkg "httrack" "www-client/httrack"
+echo ""
